2ba9aa0d16824f634f0496879375573c1eb0f09c
SolidSalad <grimmieboy17@gmail.com>
2024-01-03 12:40:40 +0100

    implemented timer script -- deleted non-generic

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI interactionText; // Reference to TextMeshProUGUI component
+    public TextMeshProUGUI timerDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InteractionTextActive(bool active, string text = "e")
+    {
+        interactionText.text = $"[{text}]";
+        interactionText.gameObject.SetActive(active);
+    }
+
+    public void UpdateTimer(string text, string value)
+    {
+        timerDisplay.text = $"{text}: {value}";
+    }
+
+    public void TimerDisplayActive(bool active)
+    {
+        timerDisplay.gameObject.SetActive(active);
+    }
+}
