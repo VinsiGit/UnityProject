90101ac1c2a2467d6629b75ad5550eb080316967
SolidSalad <grimmieboy17@gmail.com>
2024-01-17 18:41:04 +0100

    finished dialogue logic -- created pretty UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,8 +9,10 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI timerDisplay;
     public TextMeshProUGUI progressDisplay;
     public TextMeshProUGUI goalDisplay;
-    public TextMeshProUGUI dialogueWindow; // New TextMeshProUGUI for dialogue
-
+    public GameObject dialogueWindow;
+    
+    private TextMeshProUGUI output_text; // New TextMeshProUGUI for dialogue
+    private TextMeshProUGUI next; // New TextMeshProUGUI for dialogue
     private float dialogueTypingDelay = 0.1f; // Typing speed for interaction text
     private Coroutine dialogueCoroutine; // Coroutine reference for interaction text
 
@@ -19,17 +21,20 @@ public class UIManager : MonoBehaviour
     {
         // Initialize interactionTextCoroutine
         dialogueCoroutine = null;
+        output_text = dialogueWindow.transform.Find("text").GetComponent<TextMeshProUGUI>();
+        next = dialogueWindow.transform.Find("next").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     // Public method to call interaction text from the outside
     public void TypeDialogue(string text)
     {
+        next.gameObject.SetActive(false);
         if (dialogueCoroutine != null)
         {
             // Stop the existing coroutine if it's running
@@ -45,25 +50,30 @@ public class UIManager : MonoBehaviour
 
     private IEnumerator TypeDialogueCoroutine(string textToWrite)
     {
-        dialogueWindow.text = ""; // Ensure the text is initially empty
+        output_text.text = ""; // Ensure the text is initially empty
 
         int characterIndex = 0;
 
         while (characterIndex < textToWrite.Length)
         {
             // Append one character to the dialogue window
-            dialogueWindow.text += textToWrite[characterIndex];
+            output_text.text += textToWrite[characterIndex];
             characterIndex++;
 
             // Wait for the specified time before typing the next character
             yield return new WaitForSeconds(dialogueTypingDelay);
         }
 
-        // Wait for the specified display time before clearing the text
-        yield return new WaitForSeconds(dialogueTypingDelay);
+        next.gameObject.SetActive(true);
 
-        // Clear the dialogue window after the display time
-        dialogueWindow.text = "";
+        // Wait until the spacebar is pressed
+        while (!Input.GetKey(KeyCode.Space))
+        {
+            yield return null;
+        }
+
+        // Clear the dialogue window after the spacebar is pressed
+        output_text.text = "";
 
         // Deactivate the dialogue window
         dialogueWindow.gameObject.SetActive(false);
